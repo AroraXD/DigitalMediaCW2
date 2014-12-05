0c4caff581e882e90d2557104727c682723f157d
yuvesh <yuv3sh@gmail.com>
2014-12-05 12:45:39 +0000

    android

---
+++ SandRunner/android/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/var/folders/9_/xfkjgcfj39j4d29qvjylws8m0000gn/T/android4720705832373780652sketch/bin/AndroidManifest.xml : \
+/var/folders/9_/xfkjgcfj39j4d29qvjylws8m0000gn/T/android4720705832373780652sketch/AndroidManifest.xml \
